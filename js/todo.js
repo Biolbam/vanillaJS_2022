a06067ae7031fac46482c6c823f0b7e36bb40101
Biolbam <34931425+Biolbam@users.noreply.github.com>
2022-02-18 21:00:28 +0900

    final

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,54 @@
-test;
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+	localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+	const li = event.target.parentElement;
+	li.remove();
+	toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+	saveToDos();
+}
+
+function paintToDo(newTodo) {
+	const li = document.createElement("li");
+	li.id = newTodo.id;
+	const span = document.createElement("span");
+	span.innerText = newTodo.text;
+	const button = document.createElement("button");
+	button.innerText = "❌";
+	button.addEventListener("click", deleteToDo);
+	li.appendChild(span);
+	li.appendChild(button);
+	toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+	event.preventDefault(); // 새로고침을 막고
+	const newTodo = toDoInput.value; // submit하는 텍스트를 저장하고  <-- input의 현재 value를 새로운 변수에 복사함.그 다음 하는것은 newToDo 변수와는 아무 상관이 없어.
+	toDoInput.value = ""; // 텍스트상자 안의 텍스트를 초기화시켰다.
+	const newTodoObj = {
+		text: newTodo,
+		id: Date.now(),
+	};
+	toDos.push(newTodoObj);
+	paintToDo(newTodoObj);
+	saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+	const parsedToDos = JSON.parse(savedToDos);
+	toDos = parsedToDos;
+	parsedToDos.forEach(paintToDo);
+}
